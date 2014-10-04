ddcd8d7d9c9beefe1c22e900cb7fe53cd9bc62c8
allenliu76310 <allenliu76310@gmail.com>
2014-10-04 20:34:10 +0800

    add button

--- src/org/NTUH/barcodereader/MainActivity.java
+++ src/org/NTUH/barcodereader/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
-
 public class MainActivity extends ActionBarActivity {
 
 	@Override
